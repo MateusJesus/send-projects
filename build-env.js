b2592f74ef7d9116f43a55cd50af1da39146c20c
Mateus de Jesus <mateus191019@gmail.com>
2025-05-25 13:47:37 -0400

    build update

---
+++ build-env.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+const content = `
+window.env = {
+  FIREBASE_API_KEY: "${process.env.FIREBASE_API_KEY}",
+  FIREBASE_AUTH_DOMAIN: "${process.env.FIREBASE_AUTH_DOMAIN}",
+  FIREBASE_PROJECT_ID: "${process.env.FIREBASE_PROJECT_ID}",
+  FIREBASE_STORAGE_BUCKET: "${process.env.FIREBASE_STORAGE_BUCKET}",
+  FIREBASE_MESSAGING_SENDER_ID: "${process.env.FIREBASE_MESSAGING_SENDER_ID}",
+  FIREBASE_APP_ID: "${process.env.FIREBASE_APP_ID}"
+};
+`;
+
+fs.writeFileSync("env.js", content);
+console.log("✅ env.js gerado com sucesso!");
